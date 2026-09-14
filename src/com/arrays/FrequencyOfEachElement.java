6c47d613ea85e091db87ae7c23adf72c512af7a1
madhu sri <nissankararaomadhusri@gmail.com>
2026-09-14 22:24:56 +0530

    Add Array Programs

---
+++ src/com/arrays/FrequencyOfEachElement.java
@@ -0,0 +1,14 @@
+package com.arrays;
+
+public class FrequencyOfEachElement {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,2,3,1,1};
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/arrays/binarysearch/PeakElement.java
@@ -0,0 +1,20 @@
+package com.arrays.binarysearch;
+
+public class PeakElement {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,3,4,5,6,7,8,5,1};
+		int n = arr.length;
+		
+//		for(int i=0;i<n;i++)
+//		{
+//		if((i==0 || arr[i-1]<arr[i])&& (i==n-1|| arr[i]>arr[i+1]))
+//		{
+//				System.out.println(i);
+//		}
+//		}
+
+	}
+
+}
